2f561d39af31527a0ce2f743fd4a4bc518c1654d
gerawortev <gerardo.chapa@wortev.com>
2021-08-16 09:11:21 -0500

    Funcionalidades calculadora

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 // Variables para slider
-var slider1 = document.getElementById("myRange1");
-var output1 = document.getElementById("value1");
-var slider2 = document.getElementById("myRange2");
-var output2 = document.getElementById("value2");
+var slider1 = document.getElementById("calc-myRange1");
+var output1 = document.getElementById("calc-cantidad");
+var slider2 = document.getElementById("calc-myRange2");
+var output2 = document.getElementById("calc-tiempo");
+
+//Variables para actualizar textos
+var inputText1 = document.getElementById("calc-input-text1");
+var inputText2 = document.getElementById("calc-input-text2");
+
+var resultText1 = document.getElementById("calc-result-text1");
+var resultText2 = document.getElementById("calc-result-text2");
+var resultText3 = document.getElementById("calc-result-text3");
+var resultText4 = document.getElementById("calc-result-text4");
+var resultText5 = document.getElementById("calc-result-text5");
+var resultText6 = document.getElementById("calc-result-text6");
+var resultText7 = document.getElementById("calc-result-text7");
+var resultText8 = document.getElementById("calc-result-text8");
+var resultTotal = document.getElementById("calc-result-total");
 
 // Variables para gráfico
-var rendBrut = 0;
-var inver = 0;
-
-var montoInv = 0;
-var plazoInv = 0;
+var montoInic = 10000;
+var plazoInv = 1;
 
 // Configuración de gráfico
 
@@ -45,9 +56,80 @@ var options = {
   },
 };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+var chart = new ApexCharts(document.querySelector("#calc-chart"), options);
 chart.render();
 
+// Control de sliders
+slider1.oninput = function () {
+    montoInic = this.value*10000;
+    var aux = (parseInt(this.value*10000)).toFixed(2);
+    output1.value = formatear(aux);
+    inputText1.innerHTML = formatear(aux);
+    calcular(montoInic, plazoInv);
+    ApexCharts.exec('mychart', 'updateSeries', [this.value, this.value, this.value], true);
+}
+slider2.oninput = function () {
+    plazoInv = this.value;
+    output2.innerHTML = this.value;
+    calcular(montoInic, plazoInv);
+}
+slider1.addEventListener("input", function(){
+    var x = slider1.value;
+    var color = 'linear-gradient(90deg, #5BE55A '+x+'%, #ffff '+ x +'%)';
+    slider1.style.background = color;
+    invWortev = x;
+})
+slider2.addEventListener("input", function(){
+    var x = slider2.value*10;
+    var color = 'linear-gradient(90deg, #5BE55A '+x+'%, #ffff '+ x +'%)';
+    slider2.style.background = color;
+    invWortev = x;
+})
+output1.onchange = function() {
+    montoInic = this.value;
+    var aux = formatear(parseInt(this.value).toFixed(2));
+    output1.value = aux;
+    calcular(montoInic, plazoInv);
+}
+
+// center message 
+var messageBox = document.getElementById('calc-result');
+var messageBoxWidth = messageBox.offsetWidth;
+var messageBoxHeight = messageBox.offsetHeight;
+
+messageBox.style.left = "calc(49% - " + messageBoxWidth / 2 + "px)";
+messageBox.style.top = "calc(50% - " + messageBoxHeight / 2 + "px)";
+// end: center message 
+
+
+//Actualización de datos en tiempo real y funcionalidad de calculadora
+function calcular(monto, tiempo){
+  if(monto>=0){
+    resultText1.innerHTML = "$  "+monto.toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText2.innerHTML = "$  "+(monto*0.02).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText3.innerHTML = "$  "+(monto*0.02*0.16).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText4.innerHTML = "$  "+(monto*0.02+monto*0.02*0.16).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText5.innerHTML = "$  "+(-monto*0.02*0.1066).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText6.innerHTML = "$  "+(-monto*0.02*0.2).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText7.innerHTML = "$  "+(-monto*0.02*0.1066-monto*0.02*0.2).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultText8.innerHTML = "$  "+(monto*0.02+monto*0.02*0.16-monto*0.02*0.1066-monto*0.02*0.2).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    resultTotal.innerHTML = "$  "+((monto*0.02+monto*0.02*0.16-monto*0.02*0.1066-monto*0.02*0.2)*12).toLocaleString('es-MX', {minimumFractionDigits: 2});
+    inputText2.innerHTML = "$  "+((monto*0.02+monto*0.02*0.16-monto*0.02*0.1066-monto*0.02*0.2)*12*tiempo).toLocaleString('es-MX', {minimumFractionDigits: 2});
+  }
+  else{
+    alert("Error al colocar tu número");
+  }
+}
+
+//Dar formato a números
+function formatear(dato) {
+  return dato.replace(/./g, function(c, i, a) {
+	return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+    });
+}
+
+
+//Alternativas calculadora
 
 //Opción 2
 // var options = {
@@ -134,46 +216,3 @@ chart.render();
 // };
 // var chart = new ApexCharts(document.querySelector("#chart"), options);
 // chart.render();
-
-
-// Control de sliders
-slider1.oninput = function () {
-    output1.innerHTML = this.value*10000;
-    montoInic = this.value*10000;
-    ApexCharts.exec('mychart', 'updateSeries', [this.value, this.value, this.value], true);
-}
-slider2.oninput = function () {
-    output2.innerHTML = this.value*1000;
-    depositMens = this.value*1000
-}
-slider1.addEventListener("input", function(){
-    var x = slider1.value;
-    var color = 'linear-gradient(90deg, #5BE55A '+x+'%, #A7FFA4 '+ x +'%)';
-    slider1.style.background = color;
-    invWortev = x;
-})
-slider2.addEventListener("input", function(){
-    var x = slider2.value;
-    var color = 'linear-gradient(90deg, #5BE55A '+x+'%, #A7FFA4 '+ x +'%)';
-    slider2.style.background = color;
-    invWortev = x;
-})
-
-
-// center message 
-var messageBox = document.getElementById('calc_result');
-var messageBoxWidth = messageBox.offsetWidth;
-var messageBoxHeight = messageBox.offsetHeight;
-
-messageBox.style.left = "calc(49% - " + messageBoxWidth / 2 + "px)";
-messageBox.style.top = "calc(50% - " + messageBoxHeight / 2 + "px)";
-// end: center message 
-
-
-
-
-
-// Actualización de valores en tiempo real
-// output1.innerHTML = slider1.value;
-// output2.innerHTML = slider2.value;
-// output3.innerHTML = slider3.value;
